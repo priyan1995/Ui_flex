4d7f08d2d82ed45002fcb5ff4daa1188252a25c6
Priyan Darshana <45042030+priyan1995@users.noreply.github.com>
2025-02-16 14:06:33 +0000

    intial commit

---
+++ src/components/FlexBox.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export const FlexBox = () => {
+  return (
+    <>
+      <div className="pd-flexbox-wrapper">
+        <div className="pd-flexbox-item"></div>
+        <div className="pd-flexbox-item"></div>
+        <div className="pd-flexbox-item"></div>
+        <div className="pd-flexbox-item"></div>
+      </div>
+    </>
+  );
+};
