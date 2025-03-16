ed9eb9d9f9b6d38c6d54aa18dd585123b1c836ce
Priyan Darshana <45042030+priyan1995@users.noreply.github.com>
2025-03-16 12:16:57 +0000

    dark mode function

--- src/App.js
+++ src/App.js
@@ -2,6 +2,16 @@ import { FlexBox } from "./components/FlexBox";
 import "./styles.css";
 
 export default function App() {
+  const setDarkMode = () => {
+    document.querySelector("body").setAttribute("data-theme", "dark");
+  };
+
+  const setLightMode = () => {
+    document.querySelector("body").setAttribute("data-theme", "light");
+  };
+
+  setDarkMode();
+
   return (
     <div className="App">
       <FlexBox />
